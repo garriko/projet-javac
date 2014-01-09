410b1a6ad3f73e1f6deec8d6119884b22985ea5f
Alroad <alroad1234@hotmail.fr>
2014-01-09 15:41:42 +0100

    titres

--- src/test/java/org/jacademie/FileReader/TestRepositoryReader.java
+++ src/test/java/org/jacademie/FileReader/TestRepositoryReader.java
@@ -1,6 +1,6 @@
 package org.jacademie.FileReader;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,39 +9,47 @@ import java.util.HashMap;
 import org.junit.Test;
 
 public class TestRepositoryReader {
-	
+
 	@Test
 	public void TestMusicUnFichier() throws FileNotFoundException, IOException{
-	RepositoryReader test= new RepositoryReaderImpl();
-	HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicUnFichier");
-	String in = "CodeArtiste2, NomArtiste2, CodeAlbum2, NomAlbum2, NumeroChanson2, TitreChanson2, DureeChanson2\nCodeArtiste3, NomArtiste3, CodeAlbum3, NomAlbum3, NumeroChanson3, TitreChanson3, DureeChanson3\n";
-	assertEquals(in,read.get("exemple.music"));
-	}
-	
-	@Test
-	public void TestMusicUnFichierEtUnMauvaisFichier() throws FileNotFoundException, IOException{
-	RepositoryReader test= new RepositoryReaderImpl();
-	HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicUnFichierEtUnMauvaisFichier");
-	String in = "CodeArtiste2, NomArtiste2, CodeAlbum2, NomAlbum2, NumeroChanson2, TitreChanson2, DureeChanson2\nCodeArtiste3, NomArtiste3, CodeAlbum3, NomAlbum3, NumeroChanson3, TitreChanson3, DureeChanson3\n";
-	assertEquals(in,read.get("exemple.music"));
-	}
-	
-	@Test
-	public void TestMusicDeuxFichiers() throws FileNotFoundException, IOException{
-	RepositoryReader test= new RepositoryReaderImpl();
-	HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicDeuxFichiers");
-	String in = "Contenu fichier un\nContenu fichier deux\n";
-	String conc = read.get("exemple.music").toString();
-	conc = conc.concat(read.get("exemple2.music").toString());
-	assertEquals(in,conc);
-	
-	}
-	
-	@Test
-	public void TestMusicDeuxMauvaisFichiers() throws FileNotFoundException, IOException{
-	RepositoryReader test= new RepositoryReaderImpl();
-	HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicDeuxMauvaisFichiers");
-	assertEquals(read.size(),0);	
+		RepositoryReader test= new RepositoryReaderImpl();
+		HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicUnFichier");
+		String in = "CodeArtiste2, NomArtiste2, CodeAlbum2, NomAlbum2, NumeroChanson2, TitreChanson2, DureeChanson2\nCodeArtiste3, NomArtiste3, CodeAlbum3, NomAlbum3, NumeroChanson3, TitreChanson3, DureeChanson3\n";
+		assertEquals(in,read.get("exemple.music"));
 	}
 
+	@Test
+	public void TestMusicUnFichierEtUnMauvaisFichier() throws FileNotFoundException, IOException{
+		RepositoryReader test= new RepositoryReaderImpl();
+		HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicUnFichierEtUnMauvaisFichier");
+		String in = "CodeArtiste2, NomArtiste2, CodeAlbum2, NomAlbum2, NumeroChanson2, TitreChanson2, DureeChanson2\nCodeArtiste3, NomArtiste3, CodeAlbum3, NomAlbum3, NumeroChanson3, TitreChanson3, DureeChanson3\n";
+		assertEquals(in,read.get("exemple.music"));
+	}
+
+	@Test
+	public void TestMusicDeuxFichiers() throws FileNotFoundException, IOException{
+		RepositoryReader test= new RepositoryReaderImpl();
+		HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicDeuxFichiers");
+		String in = "Contenu fichier un\nContenu fichier deux\n";
+		String conc = read.get("exemple.music").toString();
+		conc = conc.concat(read.get("exemple2.music").toString());
+		assertEquals(in,conc);
+
+	}
+
+	@Test
+	public void TestMusicDeuxMauvaisFichiers() throws FileNotFoundException, IOException{
+		RepositoryReader test= new RepositoryReaderImpl();
+		HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/TestMusicDeuxMauvaisFichiers");
+		assertEquals(read.size(),0);	
+	}
+
+	@Test
+	public void TestPlaylist() throws FileNotFoundException, IOException{
+		RepositoryReader test= new RepositoryReaderImpl();
+		HashMap read = test.simpleReadRepository("C:/Users/adrien/Desktop/FI3/Java Academy/Maven/projet-javac/TestRepositoryReader/Playlist");
+		//System.out.println(read.toString());
+		assertTrue(true);
+	}
 }
+
